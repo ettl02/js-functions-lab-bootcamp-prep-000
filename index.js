74c68939c3658a956424958e0dd00366d85f71c2
Emily Wessel <ettl02@gmail.com>
2018-05-10 16:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function happyHolidays (string){
   return "Happy holidays!";
 }
 function happyHolidaysTo(name){
-  return 'Happy holidays, ${name}!';
+  return console.log('Happy holidays, ${name}!');
   }
 function happyHolidayTo (holiday, name){
-  return '${holiday}, ${name}!';
+  return console.log('${holiday}, ${name}!');
 }
 function holidayCountdown (holiday, days){
   return console.log('Its ${days} days until ${holiday}!');
