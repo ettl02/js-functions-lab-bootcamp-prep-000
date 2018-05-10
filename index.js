6f94a7d14831a992582a9fff1d27eefc34187506
Emily Wessel <ettl02@gmail.com>
2018-05-10 16:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function happyHolidays(){
   return "Happy holidays!";
 }
-function happyHolidaysTo(name){
+function happyHolidaysTo(you){
   return 'Happy holidays, ${name}!';
   }
 function happyHolidayTo(holiday, name){
