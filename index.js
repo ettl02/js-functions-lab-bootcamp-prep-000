9804d47be6cb2adb983a56fcd76519b1cedb6636
Emily Wessel <ettl02@gmail.com>
2018-05-10 17:06:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(holiday, name){
   return 'Happy ${holiday}, ${name}!'
 }
 function holidayCountdown(holiday, number){
-  return 'It\'s ${days} days until ${holiday}!'
+  return 'It\'s ${number} days until ${holiday}!'
 }
